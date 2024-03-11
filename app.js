a5f8f9b28fcb1824e811f7c4b263c069441b645c
Humza920 <humza.rehman.779@gmail.com>
2024-03-10 21:23:19 -0700

    DONE

---
+++ app.js
@@ -0,0 +1,73 @@
+// CHAPTER 5
+
+// Question 1
+// const num1 = (20)
+// const num2 = (10)
+// const add = (num1+num2)
+// console.log(add);
+// document.write("Sum of 20 and 10 is " + add)
+
+// Question 2
+// for subtract
+// const num1 = (20)
+// const num2 = (10)
+// const add = (num1-num2)
+// console.log(add);
+// document.write("Subtract of 20 and 10 is " + add)
+// for multiply
+// const num1 = (20)
+// const num2 = (10)
+// const add = (num1*num2)
+// console.log(add);
+// document.write("Multiply of 20 and 10 is " + add)
+// for devide
+// const num1 = (20)
+// const num2 = (10)
+// const add = (num1/num2)
+// console.log(add);
+// document.write("Devide of 20 and 10 is " + add)
+// for modulus
+// const num1 = (20)
+// const num2 = (10)
+// const add = (num1%num2)
+// console.log(add);
+// document.write("Modulus of 20 and 10 is " + add)
+
+// Question 3
+// var humza = (undefined)
+// console.log(humza);
+// document.write("Value of declared variable is: " + humza + "<br>")
+// humza =(5)
+// console.log(humza);
+// document.write("Initial value: " + humza + "<br>")
+// humza = (++humza)
+// console.log(humza);
+// document.write("Value after increament is: " + humza + "<br>")
+// humza = (humza+7)
+// console.log(humza);
+// document.write("Value after addition is: " + humza + "<br>")
+// humza = (--humza)
+// console.log(humza);
+// document.write("Value after decreament is: " + humza + "<br>")
+// humza = (humza%3)
+// console.log(humza);
+// document.write("The remainder is: " + humza + "<br>")
+
+// Question 4
+// var cost = (600)
+// console.log(cost);
+// cost = (600*5)
+// console.log(cost);
+// document.write("Total cost to buy five tickets to a movie =" + cost +"PKR")
+
+// Question 5
+// var celsiusTemperature = (32);
+// console.log(celsiusTemperature);
+// var chToF = ((celsiusTemperature*9/5)+32);
+// console.log(chToF);
+// document.write(celsiusTemperature + "C " + "is = to " + chToF + "F" + "<br>" );
+// var ferenTemperature = (67); 
+// console.log(ferenTemperature);
+// var fToCh = ((ferenTemperature-32)*5/9);
+// console.log(fToCh);
+// document.write(ferenTemperature + "F " + "is = to " + fToCh + "C" + "<br>" );
